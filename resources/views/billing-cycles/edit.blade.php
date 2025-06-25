5ebbe7a465ffd31c347c2374a44c03985066cad4
Omer Fayyaz <omerfayyaz.engr@gmail.com>
2025-06-25 14:49:47 +0500

    Fixes

--- resources/views/billing-cycles/edit.blade.php
+++ resources/views/billing-cycles/edit.blade.php
@@ -129,7 +129,7 @@
                             <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
                             </svg>
-                            Update Cycle
+                            Update
                         </button>
                     </div>
                 </div>
